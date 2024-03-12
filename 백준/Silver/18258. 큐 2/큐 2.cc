713777a5e41cd3a98af473fa692a0f4f2d24b7f2
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-03-12 17:16:23 +0900

    [Silver IV] Title: 큐 2, Time: 388 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+queue <int> a;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	while(N--){
+		string op;
+		cin >> op;
+	
+		if (op == "push"){
+			int tmp; cin >> tmp;
+			a.push(tmp);
+		}
+		else if (op == "pop"){
+			if (a.size() == 0) cout << -1 << '\n';
+			else {
+				cout << a.front() << '\n';
+				a.pop();
+			}
+		}
+		else if (op == "size") cout << a.size() << '\n';
+		else if (op == "empty") cout << (a.size()==0) << '\n';
+		else if (op == "front") {
+			if (a.size() == 0) cout << -1 << '\n';
+			else cout << a.front() << '\n';
+		}
+		else {
+			if (a.size() == 0) cout << -1 << '\n';
+			else cout << a.back() << '\n';
+		}
+	
+	}
+}

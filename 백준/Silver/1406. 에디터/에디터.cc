967d95397f0a9d3030d915c55ad3ee54e2dbaa20
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-03-14 11:40:51 +0900

    [Silver II] Title: 에디터, Time: 144 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,64 +1,28 @@
-#if 0 // 1620
-#include <bits/stdc++.h>
-using namespace std;
-
-map<string, int> mp;
-map<int, string> mp2;
-int N,M;
-
-int main(){
-	ios::sync_with_stdio(0);	cin.tie(0);
-	cin >> N >> M;
-	
-	for (int i=1; i<=N; i++){
-		string tmp; cin >> tmp;
-		mp[tmp] = i;
-		mp2[i] = tmp;
-	}
-	
-	for (int i=0; i<M;i++){
-		string tmp;	cin >> tmp;
-		if (atoi(tmp.c_str()))	cout << mp2[atoi(tmp.c_str())] << '\n';			
-		else cout << mp[tmp] << '\n';
-	}	
-}
-#endif
-
-#if 1
 #include <bits/stdc++.h>
 using namespace std;
 list <char> a;
 string input;
-
+int M;
 int main(){
 	cin >> input;
-	for (int i=0; i<input.size(); i++){
-		a.push_back(input[i]);
-	}
-	
-	auto cur = a.end();
-	int N;
-	cin >> N;
-	for (int i=0; i<N; i++){
-		char tmp;
-		cin >> tmp;
-		switch (tmp) {
-			case 'L': if (cur != a.begin()) cur--;
-					  break;
-			case 'D': if (cur != a.end()) cur++;
-					  break;
-			case 'B': if (cur != a.begin()) { 
-						cur--;
-					  	cur = a.erase(cur);
-					  }
-					  break;
-			case 'P': char tmp2;
-					  cin >> tmp2;
-					  a.insert(cur, tmp2);
-					  break;
-			default : break;
+	for (int i=0; i<input.size(); i++) a.push_back(input[i]);
+	cin >> M;
+	auto it = a.end();
+	while(M--){
+		char tmp; cin >> tmp;
+		if (tmp == 'L'){
+			if (it != a.begin()) it--; 
 		}
-	}	
+		else if (tmp == 'D') {
+			if (it != a.end()) it++; 		
+		}
+		else if (tmp == 'B') {
+			if (it != a.begin()) {it--; it = a.erase(it);} 			
+		}
+		else {
+			char tmp; cin >> tmp;
+			a.insert(it, tmp); 					
+		}
+	}
 	for (auto i:a) cout << i;
 }
-#endif

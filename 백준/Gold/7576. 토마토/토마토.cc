32985ca1045e13235a6b33df60eafdd0d66be9e8
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-03-18 17:42:46 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -2,43 +2,47 @@
 using namespace std;
 #define X first
 #define Y second
-
 int board[1002][1002];
-int n,m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int day[1002][1002];
-
-int result;
+int ripe[1002][1002];
+int n,m,result;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	cin >> m >> n;
-	
-	queue<pair<int, int>> Q;
+	queue<pair<int,int>> Q;
 	for (int i=0; i<n; i++){
-		for (int j=0; j<m; j++) {
+		for (int j=0; j<m; j++){
 			cin >> board[i][j];
-			if (board[i][j]==1)	Q.push({i,j});
-			else day[i][j] = -1;
-		}
+			if (board[i][j] == 1) {
+				ripe[i][j] = 0;
+				Q.push({i,j});
+			}
+			else ripe[i][j] = -1;
+		}	
 	}
-									
+
 	while(Q.size()){
 		pair<int,int> cur = Q.front(); Q.pop();
-		for (int dir=0; dir<4; dir++){
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
-			if (nx >= n || nx < 0 || ny >= m || nx < 0) continue;
-			if (day[nx][ny] != -1 || board[nx][ny]==-1) continue;
+		for(int i=0; i<4; i++){
+			int nx = cur.X + dx[i];
+			int ny = cur.Y + dy[i];
+			
+			if (nx>=n || ny>=m || nx<0 || ny<0) continue;
+			if (board[nx][ny]==-1 || ripe[nx][ny]!=-1) continue;
 			Q.push({nx,ny});
-			day[nx][ny] = day[cur.X][cur.Y] + 1;
-			result = max(result, day[nx][ny]);
+			ripe[nx][ny] = ripe[cur.X][cur.Y] + 1;
+			result = ripe[nx][ny];
 		}
 	}
-	for (int i=0; i<n; i++)
-		for (int j=0; j<m; j++)
-			if (board[i][j]==0 && day[i][j]==-1) result = -1;						
-	
+
+	for (int i=0; i<n; i++){
+		for (int j=0; j<m; j++){
+			//cout << board[i][j] << ' ';
+			if (board[i][j]==0 && ripe[i][j]==-1) result = -1;
+		}
+//		cout << endl;
+	}
 	cout << result;
 }

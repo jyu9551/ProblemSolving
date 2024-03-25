7026a4dcdafe4f2e3478c27334982e857dc36bee
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-03-25 22:19:56 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+long long a,b,c;
 
-long long go(long long a, long long b, long long c){
+long long power(long long b) {
 	if (b == 0) return 1;
-	if (b == 1) return a%c;
-	if (b%2==0) return go(a, b/2, c)%c * go(a, b/2, c)%c;	
-	else return go(a,b/2, c)%c * go(a,b/2+1,c)%c;
+	if (b == 1) return a % c;
+	
+	if (b % 2 == 0) return power(b/2) % c * power(b/2) % c;
+	return power(b/2) % c * power(b/2) % c * a % c;
 }
-
 int main(){
-	long long A,B,C;
-	cin >> A >> B >> C;
-	cout << go(A,B,C);
+	cin >> a >> b >> c;
+	cout << power(b);
 }

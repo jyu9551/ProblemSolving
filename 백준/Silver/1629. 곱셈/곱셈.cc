1db3d069189c4ee0b6acd8f9a29adcc31c241990
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-03-25 22:22:36 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long a,b,c;
+long long a,b,c,k;
 
 long long power(long long b) {
 	if (b == 0) return 1;
 	if (b == 1) return a % c;
 	
-	if (b % 2 == 0) return power(b/2) % c * power(b/2) % c;
-	return power(b/2) % c * power(b/2) % c * a % c;
+	k = power(b / 2) % c;
+	if (b % 2 == 0) return k * k % c;
+	return k * k % c * a % c;
 }
 int main(){
 	cin >> a >> b >> c;

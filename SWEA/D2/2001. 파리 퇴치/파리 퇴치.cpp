57c2741920341081e18289e754b885e37046e647
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-04-29 16:10:30 +0900

    [D2] Title: 파리 퇴치, Time: 7 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int T; cin >> T;
+	for (int t=0; t<T; t++){
+		int table[16][16] = {0, };
+		int N, M; cin >> N >> M;
+		int mx = -1;
+		for (int i=0; i<N; i++){
+			for (int j=0; j<N; j++){
+				int tmp; cin >> tmp;
+				table[i][j] = tmp;
+			}
+		}
+		int table2[N-M+1][N-M+1];
+		for(int i=0; i<N-M+1; i++)
+			for(int j=0; j<N-M+1; j++) table2[i][j]=0;
+		
+		
+		for(int i=0; i<N-M+1; i++) {
+			for(int j=0; j<N-M+1; j++){
+		
+				for (int m=0; m<M; m++){
+					for (int n=0; n<M; n++){
+						table2[i][j] += table[i+m][j+n];
+					}
+				}
+				mx = max(mx, table2[i][j]);
+			}
+		}
+		cout << '#' << t+1 << ' ' << mx << '\n';
+	}
+}

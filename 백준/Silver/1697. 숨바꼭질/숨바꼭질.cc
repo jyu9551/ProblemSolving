f164d74555a57e2b3e6a8f929f96a02e2b6c1b53
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-07-11 23:16:46 +0900

    [Silver I] Title: 숨바꼭질, Time: 12 ms, Memory: 10888 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,42 +1,40 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-int table[1000002];
-int   vis[1000002];
+int board[1000000];
+int vis[1000000];
 int N,K;
-int dx[2] = {-1,1};
+int dx[2]={-1,1};
 
 int main(){
 	ios::sync_with_stdio(0);
-    cin >> N >> K;
-    queue<int> Q;
-	Q.push(N);	
+	cin >> N >> K;
+	queue<int> Q;
+	Q.push(N);
 	vis[N] = 1;
-		
-	while(Q.size()){
+	
+	while(Q.size()){		
 		int cur = Q.front(); Q.pop();
-		
-        for (int i=0; i<2; i++){
-    		int nx = cur + dx[i];		
-    		if (nx<0 || nx>=1e6) continue;
-    		if (vis[nx] != 0) continue;		
-    		if (nx == K) {
-    			cout << vis[cur];
-    			return 0;
-    		}
-    		Q.push(nx); vis[nx] = vis[cur] + 1;
-        }
-        
-		int nx = cur*2;		
+		for (int i=0; i<2; i++){
+			int nx = cur + dx[i];
+			if (nx<0 || nx>=1e6) continue;
+			if (vis[nx] != 0) continue;
+			if (nx == K) {
+				cout << vis[cur];
+				exit(0);	
+			}
+			Q.push(nx);
+			vis[nx] = vis[cur] + 1;
+		}	
+		int nx = cur*2;
 		if (nx<0 || nx>=1e6) continue;
-		if (vis[nx] != 0) continue;	
+		if (vis[nx] != 0) continue;
 		if (nx == K) {
 			cout << vis[cur];
-			return 0;
+			exit(0);	
 		}
-		Q.push(nx); vis[nx] = vis[cur] + 1;					
+		Q.push(nx);
+		vis[nx] = vis[cur] + 1;
 	}
-	
-	
 	cout << 0;
 }

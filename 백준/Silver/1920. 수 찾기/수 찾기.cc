958f28f78bd61e4fd8675d7ae75312fa7880fc6b
kugnuoy <73926395+jyu9551@users.noreply.github.com>
2024-09-21 18:48:01 +0900

    [Silver IV] Title: 수 찾기, Time: 92 ms, Memory: 9152 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,19 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
-
-map<int,int> mp;
+map <int,int> mp;
 
 int main(){
-	ios::sync_with_stdio(0);	cin.tie(0);
-	int N;	cin >> N;
-	while(N--){
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N; cin >> N;
+	while(N--) {
 		int tmp; cin >> tmp;
-		mp.insert({tmp, 1});
+		mp.insert({tmp, 1});			
 	}
 	
-	int M;	cin >> M;
-	while(M--){
+	int M; cin >> M;
+	while(M--) {
 		int tmp; cin >> tmp;
-		cout << mp[tmp] << '\n';
+		cout << mp[tmp] << '\n';			
 	}
+
+
+
 }
